6b08d1c8772f95a7579f303e63a47c1b04238f46
nayeongKim <skdud4659@gmail.com>
2022-06-08 01:04:16 +0900

    [+] - ch01_출력하기.

---
+++ ch01/ex01.cpp
@@ -0,0 +1,16 @@
+// import > stdio(standard input and output).h 라이브러리 : 표준 입출력
+#include <stdio.h>
+
+// main 함수 정의
+// main 함수 : 프로그램의 시작과 끝을 알려주는 함수 > 프로그램이 시작되면 실행되고 함수가 종료되면 프로그램이 종료됨 > 반드시 main이라는 함수가 존재해야함.
+int main()
+{
+  // 프린트
+  printf("hello, world!\n");
+
+  // 문제 1
+  printf("My name is Doodle\n");
+
+  // 문제 2
+  printf("동해물과 백두산이\n마르고\n닳도록\n");
+}
