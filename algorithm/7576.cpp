d6b6b442346c565354645d7d330560fc64b4bccc
skdud4659 <skdud4659@gmail.com>
2022-07-20 00:31:40 +0900

    [fix] - 알고리즘 백준 7576 수정.

--- algorithm/7576.cpp
+++ algorithm/7576.cpp
@@ -7,7 +7,9 @@ int N, M;
 int arr[1001][1001];
 bool visit[1001][1001];
 
+// 좌우 좌표
 int v1[4] = {-1, 0, 1, 0};
+// 상하 좌표
 int v2[4] = {0, 1, 0, -1};
 
 queue<pair<int, int>> que;
@@ -15,16 +17,20 @@ queue<pair<int, int>> que;
 
 void bfs() {
     
-    while(que.empty() == 0) {
+    while(!que.empty()) {
+        // 큐의 현재 원소(익은 토마토(1))를 꺼내기
         int x = que.front().first;
         int y = que.front().second;
         que.pop();
         
+        // 주변 탐색
         for(int i = 0; i < 4; i++) {
             int xx = v1[i] + x;
             int yy = v2[i] + y;
             
+            // 지도 범위 내 체크
             if(xx >= 0 && xx < N && yy >= 0 && yy < M) {
+                // 칸이 비어있지 않고 아직 방문하지 않은 좌표라면
                 if(arr[xx][yy] != -1 && !visit[xx][yy]) {
                     arr[xx][yy] = arr[x][y] + 1;
                     visit[xx][yy] = true;
@@ -58,6 +64,7 @@ int main() {
     
     cin >> M >> N;
     
+    // 그래프 만들기
     for(i = 0; i < N; i++) {
         for(j = 0; j < M; j++) {
             cin >> arr[i][j];
